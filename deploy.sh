d7065df1e1b0e9f126a97ac184944613db82f6a7
cyking7 <765166601@qq.com>
2018-06-14 15:19:52 +0800

    not update order to root

--- deploy.sh
+++ deploy.sh
@@ -21,15 +21,10 @@ mvn clean install
 killTomcat
 
 # 删除原有工程
-rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
-
-cd $TOMCAT_APP_PATH/webapps/
-mv order.war ROOT.war
 
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
